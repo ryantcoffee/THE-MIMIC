f680a37480f490577d2f127072141bc78942e470
ryantcoffee <iamryant2@gmail.com>
2026-02-17 13:59:18 -0500

    Power system implemented

--- Assets/Scripts/power.cs
+++ Assets/Scripts/power.cs
@@ -1,46 +1,45 @@
-// using JetBrains.Annotations;
-// using UnityEngine;
+using JetBrains.Annotations;
+using UnityEngine;
 
-// public class power : MonoBehaviour
-// {
-//     public Clock clock;
-//     int second = 0;
-//     int prevSecond = 0;
-//     int powerLoseCounter = 0;
-//     int powerLoseRate = 20;
+public class power : MonoBehaviour
+{
+    public Clock clock;
+    int prevSecond = 0;
+    int powerLoseCounter = 0;
+    int powerLoseRate = 20;
 
-//     int Power = 100;
+    int Power = 100;
 
-//     // Start is called once before the first execution of Update after the MonoBehaviour is created
-//     void Start()
-//     {
-//         clock = clock.GetComponent<Clock>();
-//         second = clock.seconds;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        clock = clock.GetComponent<Clock>();
+       
         
         
-//     }
+    }
 
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         prevSecond = clock.seconds;
-//         losePower();
-//     }
+    // Update is called once per frame
+    void Update()
+    {
+      
+        losePower();
+        
+    }
 
-//     void losePower()
-//     {
-//         if (second != prevSecond)
-//         {
-//             prevSecond = clock.seconds;
-//             powerLoseCounter++;
-//             second++;
+    void losePower()
+    {
+        if (clock.seconds != prevSecond)
+        {
+            prevSecond = clock.seconds;
+            powerLoseCounter++;
 
-//             if (powerLoseCounter == powerLoseRate)
-//             {
-//                 Power--;
-//                 powerLoseCounter = 0;
-//                 Debug.Log(Power);
-//             }
-//         }
-//     }
-// }
+            if (powerLoseCounter == powerLoseRate)
+            {
+                Power--;
+                powerLoseCounter = 0;
+                Debug.Log(Power);
+            }
+        }
+    }
+}
